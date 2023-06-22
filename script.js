3358bee6bbf5b3322dc78eec2c01fe09b21b938e
Tanya N <134557318+tnashconsult@users.noreply.github.com>
2023-06-22 03:54:14 +0000

    Update js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const secondHand = document.querySelector('.second-hand');
 
     const seconds = now.getSeconds();
 
-    const secondsDegrees = ((hour / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
@@ -24,7 +24,7 @@ const secondHand = document.querySelector('.second-hand');
 
     // change seconds to hours on the line below
 
-    const hourDegrees = ((seconds / 12) * 360) + ((mins/60)*30) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
 
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
